c81f7fe5758f1608724f280c52f3732363b68368
internetword <98370172+internetword@users.noreply.github.com>
2022-02-14 10:51:52 -0500

    a

--- main.js
+++ main.js
@@ -15,19 +15,30 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result () {
-   
-   
+   let newStory = storyText;
+   let xItem = randomValueFromArray(insertX);
+   let yItem = randomValueFromArray(insertY);
+   let zItem = randomValueFromArray(insertZ);
+
+   newStory = newStory.replaceAll(':insertx:', xItem);
+   newStory = newStory.replaceAll(':inserty:', yItem);
+   newStory = newStory.replaceAll(':insertz:', zItem);
+
     if (customName.value !== '') {
         const name = customName.value;
+        newStory.replace('Bob', name);
 
     }
 
     if (document.getElementById('uk').checked) {
-        const weight = Math.round(300);
-        const temperature = Math.round(94);
+        const weight = Math.round(300 / 14) + ' stones';
+        const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+
+        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('94 fahrenheit', temperature);
 
     }
 
-    story.textContent;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
 }
