ee544520ccafe0b9978bd1245c7867fb6818bfa8
internetword <98370172+internetword@users.noreply.github.com>
2022-12-19 08:42:14 -0500

    g

---
+++ script.js
@@ -0,0 +1,13 @@
+let p = document.querySelector('p');
+
+function Stime (duration) {
+    
+}
+
+function Stopwatch () {
+    let start, end, duration = 0;
+
+    this.start = function () {
+        start = Date.now();
+    }
+}
